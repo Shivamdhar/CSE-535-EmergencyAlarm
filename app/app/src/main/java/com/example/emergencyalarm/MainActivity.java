f25590db76720608b0d49342be5f78dd58821a41
Shreyas <srdevan@asu.edu>
2019-11-09 22:13:14 -0700

    adds the upload functionality code (TODO: test upload)

--- app/app/src/main/java/com/example/emergencyalarm/MainActivity.java
+++ app/app/src/main/java/com/example/emergencyalarm/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.emergencyalarm;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -11,8 +14,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -21,8 +34,10 @@ import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
+    private Uri fileUri;
     int count = 1;
     private Button startbtn;
+    private Button uploadbtn;
     private MediaRecorder mRecorder;
     private static final String LOG_TAG = "AudioRecording";
     private static String mFileName = null;
@@ -32,15 +47,144 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startbtn = findViewById(R.id.recordButton);
 
+        startbtn = findViewById(R.id.recordButton);
         startbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 recordAudio();
             }
         });
+
+        uploadbtn = findViewById(R.id.uploadButton);
+        uploadbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UploadTask up1 = new UploadTask();
+                Toast.makeText(getApplicationContext(),"Stating to Upload",Toast.LENGTH_LONG).show();
+                up1.execute();
+            }
+        });
     }
+
+
+//    UPLOAD AUDIO METHODS =========================================================================
+
+
+    public class UploadTask extends AsyncTask<String, String, String> {
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+
+                String url = "http://10.218.107.121/cse535/upload_video.php";
+                String charset = "UTF-8";
+                String group_id = "40";
+                String ASUid = "1200072576";
+                String accept = "1";
+
+                System.out.println(fileUri.getPath());
+                File audioFile = new File(fileUri.getPath());
+//                File videoFile = new File(Environment.getExternalStorageDirectory()+"/my_folder/Action1.mp4");
+                String boundary = Long.toHexString(System.currentTimeMillis()); // Just generate some unique random value.
+                String CRLF = "\r\n"; // Line separator required by multipart/form-data.
+
+                URLConnection connection;
+
+                connection = new URL(url).openConnection();
+                connection.setDoOutput(true);
+                connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+                try (
+                        OutputStream output = connection.getOutputStream();
+                        PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
+                ) {
+                    // Send normal accept.
+                    writer.append("--" + boundary).append(CRLF);
+                    writer.append("Content-Disposition: form-data; name=\"accept\"").append(CRLF);
+                    writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+                    writer.append(CRLF).append(accept).append(CRLF).flush();
+
+                    // Send normal accept.
+                    writer.append("--" + boundary).append(CRLF);
+                    writer.append("Content-Disposition: form-data; name=\"id\"").append(CRLF);
+                    writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+                    writer.append(CRLF).append(ASUid).append(CRLF).flush();
+
+                    // Send normal accept.
+                    writer.append("--" + boundary).append(CRLF);
+                    writer.append("Content-Disposition: form-data; name=\"group_id\"").append(CRLF);
+                    writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+                    writer.append(CRLF).append(group_id).append(CRLF).flush();
+
+
+                    // Send audio file.
+                    writer.append("--" + boundary).append(CRLF);
+                    writer.append("Content-Disposition: form-data; name=\"uploaded_file\"; filename=\"" + audioFile.getName() + "\"").append(CRLF);
+                    writer.append("Content-Type: audio/3gp; charset=" + charset).append(CRLF); // Text file itself must be saved in this charset!
+                    writer.append(CRLF).flush();
+                    FileInputStream vf = new FileInputStream(audioFile);
+                    try {
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = 0;
+                        while ((bytesRead = vf.read(buffer, 0, buffer.length)) >= 0)
+                        {
+                            output.write(buffer, 0, bytesRead);
+
+                        }
+                        //   output.close();
+                        //Toast.makeText(getApplicationContext(),"Read Done", Toast.LENGTH_LONG).show();
+                    }catch (Exception exception)
+                    {
+
+
+                        //Toast.makeText(getApplicationContext(),"output exception in catch....."+ exception + "", Toast.LENGTH_LONG).show();
+                        Log.d("Error", String.valueOf(exception));
+                        publishProgress(String.valueOf(exception));
+                        // output.close();
+
+                    }
+
+                    output.flush(); // Important before continuing with writer!
+                    writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
+
+
+                    // End of multipart/form-data.
+                    writer.append("--" + boundary + "--").append(CRLF).flush();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // Request is lazily fired whenever you need to obtain information about response.
+                int responseCode = ((HttpURLConnection) connection).getResponseCode();
+                System.out.println(responseCode); // Should be 200
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+            Toast.makeText(getApplicationContext(), "In Background Task " + text[0], Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+
+
+
+
+
+
+//    RECORD AUDIO METHODS =========================================================================
 
     private void recordAudio() {
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -70,6 +214,7 @@ public class MainActivity extends AppCompatActivity {
                     mRecorder = null;
                     startbtn.setEnabled(true);
                     count++;
+                    fileUri = Uri.fromFile(new File(mFileName));
                     Toast.makeText(getApplicationContext(), "Recording Stopped", Toast.LENGTH_LONG).show();
                 }
             }, 5000);
